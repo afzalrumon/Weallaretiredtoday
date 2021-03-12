8d73e0730d7af9d744a24456628e92511339f25f
afzalrumon <a.hussain.rumon@outlook.com>
2021-03-11 20:29:17 -0500

    New file added

---
+++ src/testThegit/Git.java
@@ -0,0 +1,14 @@
+package testThegit;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is me ");
+		System.out.println("we are learning git");
+		
+		
+	}
+
+}
